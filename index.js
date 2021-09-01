6e89a51d91e40b74998e55d523561bc19464c77f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-31 19:34:57 -0500

    all done with fetch challege(a,b,c)

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-const baseURL = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=200"
+const baseURL = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=200";
+const pokeBtn = document.getElementById("poke-btn");
+const list = document.getElementById("list");
+
+pokeBtn.addEventListener("click", () => {
+  //   debugger;
+  fetch(baseURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      itterateOverPoke(data["results"]);
+    });
+  function itterateOverPoke(data) {
+    // console.log(data);
+    for (poke of data) {
+      console.log(poke);
+      let li = document.createElement("li");
+      li.innerHTML = poke["name"];
+      li.addEventListener("mouseover", (event) => {
+        // debugger;
+        event.target.style.color = "green";
+      });
+      li.addEventListener("click", (event) => {
+        // debugger;
+        let li = event.target;
+        let pokeUrl = poke["url"];
+        //   console.log(pokeUrl)
+        fetch(pokeUrl)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            // console.log(data);
+            extraPokeInfo(data, li);
+          });
+      });
+      list.appendChild(li);
+      //   console.log(li);
+    }
+  }
+  function extraPokeInfo(data, li) {
+    // debugger;
+    let div = document.createElement("div");
+    let pokeId = data["id"];
+    let pokeHeight = data["height"];
+    let pokeMoveOne = data["moves"][0]["move"]["name"];
+    // console.log(pokeMoveOne, pokeHeight, pokeId)
+    div.innerHTML = `Poke ID: ${pokeId}; Poke height: ${pokeHeight}; Poke move: ${pokeMoveOne}`;
+    li.appendChild(div);
+  }
+});
